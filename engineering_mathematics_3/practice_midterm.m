9c78abcbcd58aa2c6c6e6a1600c7833b9e99ef22
Jang Inkyu <leplusbon@snu.ac.kr>
2017-10-23 17:57:22 +0900

    공수3

--- engineering_mathematics_3/practice_midterm.m
+++ engineering_mathematics_3/practice_midterm.m
@@ -67,107 +67,14 @@ end
 
 %% 하우스홀더 변환, 이중 QR 분해
 
-clear;
-
-max_iter = 5000;
-
-%A0 = 100 * (rand(100, 100) - 0.5);
+A0 = 100 * (rand(200, 200) - 0.5);
+%A0 = eye(10); 
+%A0 = [1, 1; 1, 0];
 %A0 = [3, 1, 2; -2, -5, -5; 4, 7, 1];
+%A0 = [5, -2, 1, 4; -2, 3, 0, 1; 1, 0, 2, -3; 4, 1, -3, 2];
 
-A0 = frobenius([-8000.002, 16]);
-
-n = size(A0, 1);
-
-A = A0;
-
-for i=1:(n-1)
-    gamma = sign(A(i+1, i)) * sqrt((A((i+1):n, i)' * A((i+1):n, i)));
-    u = zeros(n, 1);
-    
-    u((i+1):n) = A((i+1):n, i);
-    u(i+1) = u(i+1) + gamma;
-    
-    alpha = 1 / (gamma^2 + gamma * A(i+1, i));
-    
-    P = eye(n) - alpha * (u * u');
-    
-    A = P * A * P;
-    
-end
-
-lambda = zeros(n, 1);
-
-AA = A;
-index = 1;
-for j = 1:size(A, 2)
-    
-    if index > size(A, 2)
-        break;
-    end
-    
-    nn = n - index + 1;
-    if nn == 1
-        lambda(index) = AA(nn, nn);
-        disp(lambda(index));
-        index = index + 1;
-        AA = [];
-        small = 0;
-
-        break;
-    end
-    for i=1:max_iter
-
-        sk0 = AA(nn-1, nn-1);
-        sk1 = AA(nn, nn);
-
-        [Q, R] = qr((AA - sk0 * eye(nn)) * (AA - sk1 * eye(nn)));
-
-        AA = Q' * AA * Q;
-        
-        if i == max_iter
-            disp("did not converge");
-            break;
-        end
-        if nn > 1
-            if abs(AA(nn, nn-1)) < 10^(-14)
-                small = 1;
-                break;
-            end
-        end
-        if nn > 2
-            if abs(AA(nn-1, nn-2)) < 10^(-14)
-                small = 2;
-                break;
-            end
-        end
-        if nn == 2
-            small = 2;
-            break;
-        end
-
-    end
-    if i == max_iter
-        break;
-    end
-    if small == 1
-        lambda(index) = AA(nn, nn);
-        AA = AA(1:nn-1, 1:nn-1);
-        disp(lambda(index));
-        index = index + 1;
-    end
-    if small == 2
-        A_small = AA(nn-1:nn, nn-1:nn);
-        tr = trace(A_small);
-        de = det(A_small);
-        lambda(index) = tr / 2 + sqrt(tr^2 / 4 - de);
-        lambda(index + 1) = tr / 2 - sqrt(tr^2 / 4 - de);
-        AA = AA(1:nn-2, 1:nn-2);
-        disp(lambda(index));
-        disp(lambda(index + 1));
-        index = index + 2;
-    end
-    
-end
+lambda = eigenval(A0);
+lambda
 
 %% 프로베니우스 행렬
 
@@ -179,3 +86,88 @@ function A = frobenius(coeffs)
     A(2:n, 1:n-1) = eye(n-1);
 end
 
+%% 하우스홀더 변환
+
+function A = householder(A0)
+
+    n = size(A0, 1);
+    A = A0;
+    for i=1:(n-1)
+        gamma = sign(A(i+1, i)) * sqrt((A((i+1):n, i)' * A((i+1):n, i)));
+        u = zeros(n, 1);
+
+        u((i+1):n) = A((i+1):n, i);
+        u(i+1) = u(i+1) + gamma;
+
+        alpha = 1 / (gamma^2 + gamma * A(i+1, i));
+        if abs(gamma) < 10^-16
+            P = eye(n);
+        else
+            P = eye(n) - alpha * (u * u');
+        end
+            
+
+        A = P * A * P;
+
+    end
+end
+
+%% 이중 QR 반복법
+
+function lambda = double_qr(A0, lambda0)
+    max_iter = 10000;
+
+    nn = size(A0, 2);
+    if nn == 0
+        lambda = lambda0;
+        return;
+    end
+    if nn == 1
+        lambda = [lambda0; A0(1, 1)];
+        return;
+    end
+    if nn == 2
+        tr = trace(A0);
+        de = det(A0);
+        lambda = [lambda0; tr / 2 + sqrt(tr^2 / 4 - de)];
+        lambda = [lambda; tr / 2 - sqrt(tr^2 / 4 - de)];
+        return;
+    end
+    if nn > 2
+        for iter = 1:max_iter
+            if iter == max_iter
+                disp("did not converge");
+                lambda = lambda0;
+                return;
+            end
+            flag = 0;
+            for i = 1:nn-1
+                if abs(A0(i+1, i)) < 10^(-14)
+
+                    lambda = double_qr(A0(1:i, 1:i), lambda0);
+                    lambda = double_qr(A0(i+1:nn, i+1:nn), lambda);
+                    flag = 1;
+
+                    break;
+                end
+            end
+            if flag == 1
+                break;
+            end
+            sk0 = A0(nn-1, nn-1);
+            sk1 = A0(nn, nn);
+
+            [Q, R] = qr((A0 - sk0 * eye(nn)) * (A0 - sk1 * eye(nn)));
+
+            A0 = Q' * A0 * Q;
+        end
+    end
+end
+
+%% eigenvalues
+
+function lambda = eigenval(A0)
+    lambda = [];
+    A_householder = householder(A0);
+    lambda = double_qr(A_householder, lambda);
+end
